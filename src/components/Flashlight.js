45c6955246ebb74976fa0cb00de9d022691aa692
Maya Raman <maya.raman19@gmail.com>
2020-08-17 17:18:22 -0700

    Moved mouseevents up

--- src/components/Flashlight.js
+++ src/components/Flashlight.js
@@ -6,20 +6,29 @@ import koala from '../koala.jpg';
 
 class Flashlight extends React.Component {
 
-    hoverMouse = e => {
+    showFlashlight = () => {
         const spotlight = document.getElementById("spotlight");
-        // let diameter = parseInt(getComputedStyle(spotlight).width);
-        // console.log(diameter);
-        // diameter /= 2;
-        document.getElementById("flashlight-img").style.filter = "brightness(100%)";
+        spotlight.style.display = "block";
+    }
 
+    moveFlashlight = e => {
+        const spotlight = document.getElementById("spotlight");
+        const container = document.getElementById("container");
+        let rect = container.getBoundingClientRect();
+        console.log(rect);
         //get coordinates of mouse
-        let x = e.nativeEvent.offsetX;
-        let y = e.nativeEvent.offsetY;
+        let x = Math.round(e.clientX - rect.x);
+        let y = Math.round(e.clientY - rect.y);
         spotlight.style.left = (x) + 'px';
         spotlight.style.top = (y) + 'px';
         console.log(getComputedStyle(spotlight).left);
         console.log(getComputedStyle(spotlight).top);
+    }
+
+    hideFlashlight = () => {
+        const spotlight = document.getElementById("spotlight");
+        spotlight.style.display = "none";
+
     }
         
 
@@ -32,10 +41,12 @@ class Flashlight extends React.Component {
                     <h2>Can you figure out what's in the image below using the flashlight?</h2>
                     <p>Hover your mouse/finger over the image.</p>
                 </div>
-                <div style={{position: 'relative', width: 'max-content'}}>
+                <div style={styles.container} id="container"
+                    onMouseEnter={this.showFlashlight} onMouseMove={this.moveFlashlight} onMouseLeave={this.hideFlashlight}>
                     <div id="spotlight" style={styles.flashlight}></div>
                     {/* <div id="onTop" style={styles.cover} onMouseMove={this.hoverMouse}></div> */}
-                    <img id="flashlight-img" src={koala} alt="koala" style={styles.img} onMouseMove={this.hoverMouse}></img>
+                    <img id="flashlight-img" src={koala} alt="koala" style={styles.img} 
+                        ></img>
                 </div>
                 
             </div>
@@ -45,9 +56,14 @@ class Flashlight extends React.Component {
 }
 
 const styles = {
+    container: {
+        marginLeft: '100px', /*this is just for an offset*/
+        position: 'relative', 
+        width: 'min-content', 
+        overflow: 'hidden',
+    },
     img: {
         width: '50vw',
-        filter: 'brightness(0%)',
         zIndex: '1',
     },
     cover: {
@@ -58,6 +74,7 @@ const styles = {
         zIndex: '2',
     },
     flashlight: {
+        display: 'none', /*so it is not shown until mouseover*/
         borderRadius: '100%',
         background: 'white',
         mixBlendMode: 'difference',
@@ -65,6 +82,7 @@ const styles = {
         width: '10vw',
         height: '10vw',
         zIndex: '3',
+        transform: 'translate(-50%, -50%)',
     }
 }
 
