614def20fe11cd81b6d9beea6959c812602bb48a
Maya Raman <maya.raman19@gmail.com>
2020-08-18 14:09:57 -0700

    Moved flashlight style

--- src/components/Flashlight.js
+++ src/components/Flashlight.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import koala from '../koala.jpg';
+import './style/Flashlight.css';
 
 
 
@@ -8,7 +9,7 @@ class Flashlight extends React.Component {
 
     showFlashlight = () => {
         const spotlight = document.getElementById("spotlight");
-        spotlight.style.display = "block";
+        spotlight.style.opacity = 1;
 
         //show image after flashlight moves on
         const img = document.getElementById("flashlight-img");
@@ -19,19 +20,17 @@ class Flashlight extends React.Component {
         const spotlight = document.getElementById("spotlight");
         const container = document.getElementById("container");
         let rect = container.getBoundingClientRect();
-        console.log(rect);
+
         //get coordinates of mouse
         let x = Math.round(e.clientX - rect.x);
         let y = Math.round(e.clientY - rect.y);
         spotlight.style.left = (x) + 'px';
         spotlight.style.top = (y) + 'px';
-        console.log(getComputedStyle(spotlight).left);
-        console.log(getComputedStyle(spotlight).top);
     }
 
     hideFlashlight = () => {
         const spotlight = document.getElementById("spotlight");
-        spotlight.style.display = "none";
+        spotlight.style.opacity = 0;
 
         //hide the image after flashlight is moved off
         const img = document.getElementById("flashlight-img");
@@ -50,42 +49,15 @@ class Flashlight extends React.Component {
                     <h2>Can you figure out what's in the image below using the flashlight?</h2>
                     <p>Hover your mouse/finger over the image.</p>
                 </div>
-                <div style={styles.container} id="container"
+                <div id="container"
                     onMouseEnter={this.showFlashlight} onMouseMove={this.moveFlashlight} onMouseLeave={this.hideFlashlight}>
-                    <img id="flashlight-img" src={koala} alt="koala" style={styles.img} ></img>
-                    <div id="spotlight" style={styles.flashlight}></div>
+                    <img id="flashlight-img" src={koala} alt="koala"></img>
+                    <div id="spotlight"></div>
                 </div>
-                
             </div>
         );
     }
 
 }
-
-const styles = {
-    container: {
-        marginLeft: '100px', /*this is just for an offset*/
-        position: 'relative', 
-        overflow: 'hidden',
-        maxWidth: 'min-content',
-        maxHeight: 'min-content',
-    },
-    img: {
-        width: '50vw',
-        filter: 'brightness(0%)',
-    },
-    flashlight: {
-        display: 'none', /*so it is not shown until mouseover*/
-        borderRadius: '100%',
-        content: "",
-        position: 'absolute',
-        width: '10vw',
-        height: '10vw',
-        zIndex: '1',
-        transform: 'translate(-50%, -50%)', /*center on mouse*/
-        border: 'solid black 50vw',
-    }
-}
-
 
 export default Flashlight;
