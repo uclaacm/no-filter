c88772cad26172ae400bd40699c21162ed30e5aa
TheDarkLord247 <40956188+TheDarkLord247@users.noreply.github.com>
2020-08-20 16:12:21 +0530

    Updating index.js
    I've created a navbar as a class and am inserting my code here. I don't know if its good practice to have classes in the index file or not. My code also adds a title to the website and imports a stylesheet.

--- src/index.js
+++ src/index.js
@@ -3,9 +3,34 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import logo from './ai.png';
+import { Helmet } from 'react-helmet';
+
+class Navbar extends React.Component {
+  
+  render() {
+    return (
+        <div className = "navbar">
+          <Helmet>
+          <title>{ TITLE }</title>
+          </Helmet>
+          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap" rel="stylesheet"/>
+          <a name="top"></a>
+          <div className="topnav">
+            <a id="webtag" href="#top">Digging Deeper Into CNNs</a>
+            <a id="ACM" target="_blank" href="https://uclaacmai.github.io/">
+              <img id="logo" src={logo} alt="ACM AI Logo" />
+            ACM AI
+             </a>
+          </div>
+        </div>
+    )
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
+    <Navbar />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
