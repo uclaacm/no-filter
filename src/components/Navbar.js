c7fc7754ec08485940715e903aecc564cb56cd3a
TheDarkLord247 <40956188+TheDarkLord247@users.noreply.github.com>
2020-08-21 16:00:43 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,16 +6,14 @@ class Navbar extends React.Component {
   
     render() {
       return (
-          <div className = "navbar">
-            <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap" rel="stylesheet"/>
             <div className="topnav">
+            <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap" rel="stylesheet"/>
               <p id="webtag">Digging Deeper Into CNNs</p>
               <a id="ACM" target="_blank" rel="noopener noreferrer" href="https://uclaacmai.github.io/">
                 <img id="logo" src={logo} alt="ACM AI Logo" />
               ACM AI
                </a>
             </div>
-          </div>
       )
     }
   }
