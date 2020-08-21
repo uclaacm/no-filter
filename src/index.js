73b3bb24401d52ee06f14a8cc1cbf5a9a77b1352
TheDarkLord247 <40956188+TheDarkLord247@users.noreply.github.com>
2020-08-21 14:06:58 +0530

    Removing clutter from index.js

--- src/index.js
+++ src/index.js
@@ -5,28 +5,8 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import logo from './ai.png';
 
-class Navbar extends React.Component {
-  
-  render() {
-    return (
-        <div className = "navbar">
-          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap" rel="stylesheet"/>
-          <a name="top"></a>
-          <div className="topnav">
-            <a id="webtag" href="#top">Digging Deeper Into CNNs</a>
-            <a id="ACM" target="_blank" href="https://uclaacmai.github.io/">
-              <img id="logo" src={logo} alt="ACM AI Logo" />
-            ACM AI
-             </a>
-          </div>
-        </div>
-    )
-  }
-}
-
 ReactDOM.render(
   <React.StrictMode>
-    <Navbar />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
