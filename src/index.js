d205d28277fd6adec89043d1f07eaeec9f69fc4a
TheDarkLord247 <40956188+TheDarkLord247@users.noreply.github.com>
2020-08-21 15:08:42 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import logo from './ai.png';
 
 ReactDOM.render(
   <React.StrictMode>
