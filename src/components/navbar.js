f1ae05db108a601a56520d6d522a4f82c415d04d
TheDarkLord247 <40956188+TheDarkLord247@users.noreply.github.com>
2020-08-21 14:21:37 +0530

    Navbar component

---
+++ src/components/navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './style/Navbar.css';
+import logo from '../img/ai.png'
+
+class Navbar extends React.Component {
+  
+    render() {
+      return (
+          <div className = "navbar">
+            <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap" rel="stylesheet"/>
+            <a name="top"></a>
+            <div className="topnav">
+              <a id="webtag" href="#top">Digging Deeper Into CNNs</a>
+              <a id="ACM" target="_blank" href="https://uclaacmai.github.io/">
+                <img id="logo" src={logo} alt="ACM AI Logo" />
+              ACM AI
+               </a>
+            </div>
+          </div>
+      )
+    }
+  }
+
+export default Navbar;
