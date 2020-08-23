6b2cc4edcd30d43f6e9b7718de52c731b6af6b4b
TheDarkLord247 <40956188+TheDarkLord247@users.noreply.github.com>
2020-08-23 13:36:07 +0530

    Chnaged the navbar to a flexbox so that the navbar fits all screen sizes. Added two Poppins fonts to src/components/style. Used these fonts for the navbar instead of importing fonts to the component.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,8 +7,7 @@ class Navbar extends React.Component {
     render() {
       return (
             <div className="topnav">
-            <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap" rel="stylesheet"/>
-              <p id="webtag">Digging Deeper Into CNNs</p>
+              <a id="webtag" href ="#top">Digging Deeper Into CNNs</a>
               <a id="ACM" target="_blank" rel="noopener noreferrer" href="https://uclaacmai.github.io/">
                 <img id="logo" src={logo} alt="ACM AI Logo" />
               ACM AI
