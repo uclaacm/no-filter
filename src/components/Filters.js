87cdc4b196bb8b0ad3b94ede708275713fc74432
Maya Raman <maya.raman19@gmail.com>
2020-08-28 13:06:24 -0700

    Think I fixed pull request issues

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -2,14 +2,14 @@ import React from 'react';
 import '../App.css';
 import './style/Filters.css'
 import amsterdamRegular from '../img/filter-picker/amsterdam-regular.jpg';
-import amsterdamBlur from '../img/filter-picker/amsterdam-blur.png';
-import amsterdamBottomSobel from '../img/filter-picker/amsterdam-bottom-sobel.png';
-import amsterdamEmboss from '../img/filter-picker/amsterdam-emboss.png';
-import amsterdamLeftSobel from '../img/filter-picker/amsterdam-left-sobel.png';
-import amsterdamOutline from '../img/filter-picker/amsterdam-outline.png';
-import amsterdamRightSobel from '../img/filter-picker/amsterdam-right-sobel.png';
-import amsterdamSharpen from '../img/filter-picker/amsterdam-sharpen.png';
-import amsterdamTopSobel from '../img/filter-picker/amsterdam-top-sobel.png';
+import amsterdamBlur from '../img/filter-picker/amsterdam-blur.jpg';
+import amsterdamBottomSobel from '../img/filter-picker/amsterdam-bottom-sobel.jpg';
+import amsterdamEmboss from '../img/filter-picker/amsterdam-emboss.jpg';
+import amsterdamLeftSobel from '../img/filter-picker/amsterdam-left-sobel.jpg';
+import amsterdamOutline from '../img/filter-picker/amsterdam-outline.jpg';
+import amsterdamRightSobel from '../img/filter-picker/amsterdam-right-sobel.jpg';
+import amsterdamSharpen from '../img/filter-picker/amsterdam-sharpen.jpg';
+import amsterdamTopSobel from '../img/filter-picker/amsterdam-top-sobel.jpg';
 
 
 
@@ -18,53 +18,61 @@ class Filters extends React.Component {
         super(props);
         this.state = {
             value: 'identity',
+            src: amsterdamRegular,
+            paragraph: <>What do you think the identity filter does?</>,
         };
     }
 
     //val is string value
     changeImage = val => {
-        const filterImage = document.getElementById("filter-applied");
-        const description = document.getElementById("img-description")
+        // const filterImage = document.getElementById("filter-applied");
+        // const description = document.getElementById("img-description")
+        let src = null;
+        let paragraph = null;
         switch (val) {
             case 'identity':
-                filterImage.src = amsterdamRegular;
-                description.innerHTML = "What do you think the identity filter does?";
+                src = amsterdamRegular;
+                paragraph = <>What do you think the identity filter does?</>;
                 break;
             case 'blur':
-                filterImage.src = amsterdamBlur;
-                description.innerHTML = "We can use filters to remove the differences between pixels to <em>blur</em> an image.";
+                src = amsterdamBlur;
+                paragraph = <>We can use filters to remove the differences between pixels to <em>blur</em> an image.</>;
                 break;
             case 'bottom sobel':
-                filterImage.src = amsterdamBottomSobel;
-                description.innerHTML = "The <em>sobel</em> filter detects edges in an image.";
+                src = amsterdamBottomSobel;
+                paragraph = <>The <em>sobel</em> filter detects edges in an image.</>;
                 break;
             case 'emboss':
-                filterImage.src = amsterdamEmboss;
-                description.innerHTML = "The <em>emboss</em> filter emphasizes the difference between pixels.";
+                src = amsterdamEmboss;
+                paragraph = <>The <em>emboss</em> filter emphasizes the difference between pixels.</>;
                 break;
             case 'left sobel':
-                filterImage.src = amsterdamLeftSobel;
-                description.innerHTML = "The <em>sobel</em> filter detects edges in an image.";
+                src = amsterdamLeftSobel;
+                paragraph = <>The <em>sobel</em> filter detects edges in an image.</>;
                 break;
             case 'outline':
-                filterImage.src = amsterdamOutline;
-                description.innerHTML = "The <em>outline</em> filter emphasizes <em>large</em> differences between pixels to detect edges.";
+                src = amsterdamOutline;
+                paragraph = <>The <em>outline</em> filter emphasizes <em>large</em> differences between pixels to detect edges.</>;
                 break;
             case 'right sobel':
-                filterImage.src = amsterdamRightSobel;
-                description.innerHTML = "The <em>sobel</em> filter detects edges in an image.";
+                src = amsterdamRightSobel;
+                paragraph = <>The <em>sobel</em> filter detects edges in an image.</>;
                 break;
             case 'sharpen':
-                filterImage.src = amsterdamSharpen;
-                description.innerHTML = "The <em>sharpen</em> filter emphasizes small differences between adjacent pixels.";
+                src = amsterdamSharpen;
+                paragraph = <>The <em>sharpen</em> filter emphasizes small differences between adjacent pixels.</>;
                 break;
             case 'top sobel':
-                filterImage.src = amsterdamTopSobel;
-                description.innerHTML = "The <em>sobel</em> filter detects edges in an image.";
+                src = amsterdamTopSobel;
+                paragraph = <>The <em>sobel</em> filter detects edges in an image.</>;
                 break;
             default:
                 break;
         }
+        this.setState({
+            src: src,
+            paragraph: paragraph,
+        });
     }
 
     handleChange = event => {
@@ -77,7 +85,7 @@ class Filters extends React.Component {
     
     render() {
     return (
-        <section className="section blue" id="filters-holder">
+        <section className="section blue filters-holder">
             
             <img src={amsterdamRegular} alt="black and white amsterdam" width="500px"></img>
             
@@ -98,9 +106,10 @@ class Filters extends React.Component {
                 </label>
             </form>
             
-            <img id="filter-applied" src={amsterdamRegular} alt="amsterdam with filter applied" width="500px"></img>
-            
-            <p className="card" id="img-description">What do you think the identity filter does?</p>
+            <img id="filter-applied" src={this.state.src} alt="amsterdam with filter applied" width="500px"></img>
+            <div className="card" id="img-description">
+                <p>{this.state.paragraph}</p>
+            </div>
         </section>
     );
     }
