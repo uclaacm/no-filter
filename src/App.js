64e4bf5b63dacb848b5b87f21ec0a5df89ebd3fe
Maya Raman <41043008+mayaraman19@users.noreply.github.com>
2020-09-03 09:52:59 -0700

    Filters (#11)
    * Working image picker
    
    * attempted color changes, added paragraph tag
    
    * Some flexy stuff
    
    * Think I fixed pull request issues
    
    * Adding open source images
    
    * added information
    
    * dont even know at this point
    
    * added matrix and some links
    
    * no flex zone :0
    
    * Small className fix
    
    * I caved
    
    * Fixed up intro to example
    
    * removed Sob warning

--- src/App.js
+++ src/App.js
@@ -1,55 +1,59 @@
 import React from 'react';
 import './App.css';
 import Flashlight from './components/Flashlight.js';
+import koala from './img/koala2.jpg';
 import flamingo from './img/flamingo.jpg';
 import Navbar from './components/Navbar.js';
 import clipart from './img/people-at-computer.jpg';
+import Filters from './components/Filters.js';
 
 
 function App() {
   return (
     <div className="App">
     <Navbar />
-    <section class="blue section">
-      <div class="toc-panel">
-        <div class="mini-card">
+    <section className="blue section">
+      <div className="toc-panel">
+        <div className="mini-card">
           <h2>
             Today, we’ll be looking at Convolutional Neural Networks.
             We’ll aim to briefly answer the following three questions:
             <ol>
-              <li> What are CNNs? </li>
-              <li> How do they work? </li>
+              <li> What are CNN filters? </li>
+              <li> What do filters "see"? </li>
               <li> What are they used for? </li>
             </ol>
           </h2>
         </div>
         <div>
         </div>
-        <div class="clipart-container">
+        <div className="clipart-container">
         <img id="clipart" src={clipart} alt="People working at a computer." />
         </div>
       </div>
       </section>
-      <section class="grey section">
-        <div class="card">
+      <section className="grey section">
+        <div className="card">
         <h2>Some sort of high-level description of CNNs</h2>
         </div>
     </section>
-      <section class="blue section">
-        <div class="card">
-          <h1>Shining a Flashlight on CNNs</h1>
+      <section className="blue section">
+        <div className="card">
+          <h1>Shining a Flashlight on Filters</h1>
           <h2>Can you figure out what's in the image below using the flashlight?</h2>
 
         </div>
       </section>
-      <section class="grey section">
-        <div class="card row" id="flashlight-holder">
+      <section className="grey section">
+        <div className="card row" id="flashlight-holder">
           <p style={{alignSelf: 'center', marginRight: '1em',}}><em>Hover your mouse/finger over the image.</em></p>
           <Flashlight />
         </div>
       </section>
-      <section class="blue section">
-        <div class="card">
+      <section className="blue section">
+        
+        <div className="card">
+          <img src={koala} alt="koala in a forest" id="koala-reveal-img"></img>
           <h2>If you guessed a koala: congrats, you're (basically) a CNN!</h2>
           <p>Now take a moment to think about how you actually identified the koala. First, you probably looked at each piece—edges,
             the eyes, the nose, etc—individually.
@@ -60,15 +64,15 @@ function App() {
           <p>Finally, you were able to combine the larger components to form an answer: koala.</p>
         </div>
       </section>
-      <section class="grey section">
-        <div class="card">
+      <section className="grey section">
+        <div className="card">
           <p>Believe it or not, CNNs work the same way! A <em>filter</em> will focus on one tiny area of an
            image at a time, analyzing the picture <em>piece by piece</em>.</p>
            <p><em>Fully-connected neural networks</em> are very different. When you look at the picture below, what do you see?</p>
         </div>
       </section>
-      <section class="blue section">
-        <div class="card" id="flamingo-holder">
+      <section className="blue section">
+        <div className="card" id="flamingo-holder">
           <img src={flamingo} id="flamingo" alt="flamingo"></img>
           <div id="side-text">
             <p>You probably recognized this picture as a flamingo instantly.
@@ -77,16 +81,28 @@ function App() {
             <p>
               This is what fully-connected neural networks do.
             </p>
+
+          <p>So CNNs are different from fully-connected neural networks because <em>CNNs use filters to analyze
+             an image one feature at a time.</em></p>
+          {/* NEED TO ADD LINK */}
+          <a className="link-to-slides" href="https://www.placeholder.com" target="_blank" rel="noopener noreferrer">Before we move on, check out these slides to learn more about how filters work.</a>
           </div>
         </div>
       </section>
-      <section class="grey section">
-        <div class="card">
-          <p>So CNNs are different from fully-connected neural networks because CNNs use filters to analyze
-             an image one feature at a time.</p>
-             <h2>But how do filters actually work?</h2>
+      <section className="grey section">
+        <div className="card">
+          <h1>How do CNNs use filters to analyze images?</h1>
+          <p>
+            Filters can be used to modify the value of each pixel in an image.
+            This is how Instagram or Snapchat filters work! We'll see what actual CNNs look at in a bit. For now, however, check out how we can use filters and their values to manipulate images
+            and make features easier to see.
+          </p>
+          <p>
+            <em>In this example, select different filters to see how their values correspond to the image output. What patterns do you see?</em>
+          </p>
         </div>
       </section>
+      <Filters />
     </div>
   );
 }
