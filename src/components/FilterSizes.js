d05dd01a761b1eaaf01d8f01df27c38ca5139393
Maya Raman <maya.raman19@gmail.com>
2020-09-07 17:48:06 -0700

    starting

--- src/components/FilterSizes.js
+++ src/components/FilterSizes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import size from '../img/size.png';
+import './style/FilterSizes.css';
 
 
 class FilterSizes extends React.Component {
@@ -7,7 +8,14 @@ class FilterSizes extends React.Component {
         return (
         <section className = "grey section">
             <div className="card">
-                <img src={size} alt="Filters and layers of a CNN" id="size-img"/>
+                <h2>Click the buttons to see what's up</h2>
+                <div class="popup-container">
+                    <div className="popup" id="one"></div>
+                    <div className="popup" id="two"></div>
+                    <div className="popup" id="three"></div>
+                    <div className="popup" id="four"></div>
+                    <img src={size} alt="Filters and layers of a CNN" id="size-img"/>
+                </div>
             </div>
         </section>
         );
