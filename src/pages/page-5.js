8159f43b4b48ddc99038a35cb86277531b7327a9
Maya Raman <maya.raman19@gmail.com>
2021-05-17 16:26:00 -0700

    again merge stuff

--- src/pages/page-5.js
+++ src/pages/page-5.js
@@ -38,7 +38,23 @@ export default class Page5 extends Component {
                 <section className="section">
                     <div className="card">
                         <h1>In conclusion...</h1>
-                        <h2>idk put something here</h2>
+                        <h2>Let's revisit our three questions.</h2>
+                        <ol>
+                            <h2><li> What are CNN <em>filters</em>?</li></h2>
+                            <p>Remember the koala example? CNN filters act like flashlights.</p> 
+                            <p>They move around an image to recognize what it is.</p>
+                            <p>Go back to &lt; insert link here &gt; Shining a Flashlight on filters if you want to learn more! </p>
+                            <h2><li> What do filters <em>"see"</em>?</li></h2>
+                            <p>Filters look at <em>pixels</em> ane perform calculations.</p>
+                            <p>They "see" things like your face, and calculate values to put a Snapchat filter on your selfie.</p>
+                            <p>Check out &lt; insert link here &gt; How do we use filters to analyze images for more!</p>
+                            <h2><li> What do CNNs look like? </li></h2>
+                            <p>CNNs look like a network of filters and images, with images as inputs, filters as 'functions' and images as outputs.</p>
+                            <p>Check out visual representations of CNNs in CNN Sizes and What do CNNs Look Like?</p>
+                        </ol>
+                        <h2>Goodbye for now!</h2>
+                        <p>We hope you enjoyed our learning lab! If you have any feedback, please leave it here.</p>
+                        <p>made with love by ACM AI and ACM Teach LA</p>
                     </div>
                 </section>
             </div>
