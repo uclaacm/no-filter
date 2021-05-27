53ddfc294ab653f8fcae19f9804132a89ac06248
Nisha McNealis <nisha.mcnealis@gmail.com>
2021-05-26 19:00:06 -0700

    fix cnn carousel
    kind of

--- src/pages/page-5.js
+++ src/pages/page-5.js
@@ -1,43 +1,92 @@
 import '../App.css';
 import React, { Component } from "react";
-import Carousel from 'react-gallery-carousel';
-import 'react-gallery-carousel/dist/index.css';
+import Slider from "react-slick";
+import '../components/style/Carousel.scss';
+import f1 from '../img/panels/panel1.png';
+import f2 from '../img/panels/panel2.png';
+import f3 from '../img/panels/panel3.png';
+import f4 from '../img/panels/panel4.png';
+import f5 from '../img/panels/panel5.png';
+import f6 from '../img/panels/panel6.png';
 
-function Visual() {
-  const images = [1, 2, 3, 4, 5, 6].map((number) => ({
-  src: `https://github.com/uclaacm/no-filter/blob/master/src/img/panels/panel${number}.png`
-}));
-    return (
-      <div className="card">
-          <Carousel images={images} id="cnn-vis" />
-
-      </div>
-
-  )
-}
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
+import { faPause } from '@fortawesome/free-solid-svg-icons';
 
 export default class Page5 extends Component {
+    constructor(props) {
+      super(props);
+      this.play = this.play.bind(this);
+      this.pause = this.pause.bind(this);
+    }
+    play() {
+      this.slider.slickPlay();
+    }
+    pause() {
+      this.slider.slickPause();
+    }
     render() {
-        return (
-            <div class="page">
-                <section className="grey section">
-                    <div className="card">
-                        <h1>What do CNNs look like?</h1>
-                        <p>
-                            Now that we know how to apply filters, let's take a look at how one layer of a CNN might use two feature-detecting filters to create feature maps.
-                        </p>
+      const settings = {
+        dots: true,
+        fade: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false
+      };
+      return (
+        <div className = "page">
+            <section className="section">
+                  <div className="card">
+                      <h1>What do CNNs look like?</h1>
+                      <p>
+                          Now that we know how to apply filters, let's take a look at how one layer of a CNN might use two feature-detecting filters to create feature maps.
+                      </p>
+                  </div>
+                  <div className="card">
+                    <div className="main2">
+                      <Slider ref={slider => (this.slider = slider)} {...settings}>
+                        <div className = "frames gallery">
+                          <img src = {f1} className="frame" id="cnn-frame" alt="first example of feature pool calculation - shows how the first element of the feature map is cacluated by taking the dot product of the filter and a portion of our image"></img>
+                        </div>
+                        <div className = "frames gallery">
+                          <img src = {f2} className="frame" id="cnn-frame" alt="second example of feature pool calculation"></img>
+                        </div>
+                        <div className = "frames gallery">
+                          <img src = {f3} className="frame" id="cnn-frame" alt="third frame of feature pool calculation"></img>
+                        </div>
+                        <div className = "frames gallery">
+                          <img src = {f4} className="frame" id="cnn-frame" alt="fourth frame of feature pool calculation"></img>
+                        </div>
+                        <div className = "frames gallery">
+                          <img src = {f5} className="frame" id="cnn-frame" alt="fifth frame of feature pool calculation"></img>
+                        </div>
+                        <div className = "frames gallery">
+                          <img src = {f6} className="frame" id="cnn-frame" alt="sixth frame of feature pool calculation"></img>
+                        </div>
+                      </Slider>
+
+                      <div style={{ textAlign: "center" }}>
+                        <button className="cbutton" onClick={this.play}>
+                          <FontAwesomeIcon icon={faPlay} className ="cicon"/>
+                        </button>
+                        <button className="cbutton" onClick={this.pause}>
+                          <FontAwesomeIcon icon={faPause} className="cicon"/>
+                        </button>
+                        </div>
                     </div>
-                </section>
-                <section className="blue section">
-                    <Visual />
-                </section>
-                <section className="section">
-                    <div className="card">
-                        <h1>In conclusion...</h1>
-                        <h2>idk put something here</h2>
-                    </div>
-                </section>
-            </div>
-        )
+                  </div>
+            </section>
+
+            <section className="section">
+                <div className="card">
+                    <h1>In conclusion...</h1>
+                    <h2>idk put something here</h2>
+                </div>
+            </section>
+        </div>
+      );
     }
 }
