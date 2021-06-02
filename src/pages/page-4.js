f1805ec954e4494e7497004b3b7bafa8780efdb0
Maya Raman <maya.raman19@gmail.com>
2021-06-01 22:52:29 -0700

    Shortened words in page 4

--- src/pages/page-4.js
+++ src/pages/page-4.js
@@ -12,25 +12,25 @@ export default class Page4 extends Component {
                 <section className="grey section">
                     <div className="card">
                         <h1 class="topheader">breaking it down</h1>
-                        <h2>Look at the animation below to learn how a filter (shown as the red matrix) works on images to calculate each element of the feature map</h2>
-                        <p>(You can also explore our slides on filters (slide 28) from our <a target="_blank" class="bluelink" style={{color: '#FF00F5'}} href="https://teachla.uclaacm.com/classes/ml/10-cnn" rel="noopener noreferrer">CNN high-school lesson)</a></p>
-                        <p>Note that whenever the filter hovers over a portion of the image, the corresponding element on the
-           feature map is calculated by <em>multiplying the filter value of each cell </em> (at the bottom right of each cell) with
-           the <em> pixel value of the image's cell it is hovering over </em> and <em>adding them all together.</em></p>
+                        <h2>Let's learn how a filter calculates values.</h2>
+                        <p>Whenever the filter hovers over a portion of the image, the corresponding element on the
+           feature map is calculated by <em>multiplying</em> the filter value of each cell with
+           the <em> pixel value </em> of the cell and <em>adding</em> them all together.</p>
+           <br></br>
                         <Carousel />
                     </div>
                 </section>
 
                 <section className="blue section">
                     <div className="card">
-                        <p>The animation above shows the effects of a filter on an image in <em>grayscale</em> (black and white). A grayscale image has a depth of 1 since it just stores the black/white <em>intensity</em> (brightness) of each pixel. </p>
-                        <p> But a majority of the images taken today are in color. </p>
-                        <p> These images have multiple matrices stacked on top of each other with each matrix containing pixel values for a specific color.
-          In an <em> RGB </em> image, for example, there are 3 matrices - red, green, and blue - stacked on top of each other.
-          Thus, we say that <em> an RGB image has a depth of 3</em>.
-          The topmost channel contains the red intensity values of each pixel, followed by the middle matrix that stores information of the green intensities of each pixel and finally, the last channel that stores information about the blue intensities of each pixel.
+                        <p>The animation above shows the effects of a filter on an image in <em>grayscale</em>. A grayscale image has a depth of 1 since it just stores the black/white <em>intensity</em> of each pixel. </p>
+                        <p> But a majority of the images taken today are in color. These images usually have multiple matrices.</p>
+                        <p> In an <em> RGB </em> (<em>R</em>ed, <em>G</em>reen, <em>B</em>lue) image, for example, there are 3 matrices - red, green, and blue - stacked on top of each other.
+          Thus, we say that an RGB image has a <em> depth </em>of 3.
+          The first layer contains the red values of each pixel, the second layer contains green values, and the third layer contains blue values.
           </p>
-                        <p> With three channels instead of one, convolution with an RGB image looks like this: </p>
+                        <p> With three layers instead of one, convolution with an RGB image looks like this: </p>
+                        <br></br>
                         <img src={rgbGIF} alt="RGB filter animation" id="filterGIF" style={{ width: '400px' }} />
                     </div>
                 </section>
@@ -38,11 +38,10 @@ export default class Page4 extends Component {
                     <div className="card grid-holder">
                         <img src={depth} alt="Pixel matrices with depth of 3" className="grid-img" />
                         <div className="side-text">
-                            <p>In order to apply our filter to each channel of an <em>RGB</em> (<em>R</em>ed, <em>G</em>reen, <em>B</em>lue) image,
-            we need our filter to be 3-dimensional as well&mdash;that is, our filter should have width, height, and depth.
+                            <p>In order to apply our filter to each layer of an RGB image,
+            we need our filter to be 3-dimensional too.
             </p>
-                            <p>The width, height and depth of our filter together determine the <em>size</em> of our filter (the dimensions ). In CNNs analyzing RGB images, 3 x 3 x 3 (width x height x depth) filters are some of the most commonly-used filters,
-            along with 5 x 5 x 3 filters.</p>
+                            <p>The width, height and depth of our filter make up its <em>size</em>. In CNNs analyzing RGB images, 3 x 3 x 3 (width x height x depth) filters are some of the most commonly-used filters.</p>
                         </div>
                     </div>
                 </section>
